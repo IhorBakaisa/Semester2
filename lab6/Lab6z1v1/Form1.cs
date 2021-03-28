bb3d1390ff383dd2b290a58c97b08b784afb5b4a
IhorBakaisa <73829374+IhorBakaisa@users.noreply.github.com>
2021-03-28 19:33:50 +0300

    lab6

---
+++ lab6/Lab6z1v1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6z1v1
+{
+    public partial class Lab6z1v1 : Form
+    {
+        struct Person
+        {
+            public string passport_data;
+            public string education;
+            public string specialty;
+            public string position;
+            public int salary;
+
+            public string DisplayInfo()
+            {
+                return $"Passport_data: {passport_data}  Education: {education}  Specialty: {specialty}  Position: {position}  Salary: {salary};";
+            }
+        }
+        public Lab6z1v1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Person p1;
+            p1.passport_data = textBox1.Text;
+            p1.education = textBox2.Text;
+            p1.specialty = textBox3.Text;
+            p1.position = textBox4.Text;
+            p1.salary = Convert.ToInt32(textBox5.Text);
+            textBox11.Text = p1.DisplayInfo();
+
+            Person p2;
+            p2.passport_data = textBox6.Text;
+            p2.education = textBox7.Text;
+            p2.specialty = textBox8.Text;
+            p2.position = textBox9.Text;
+            p2.salary = Convert.ToInt32(textBox10.Text);
+            textBox12.Text = p2.DisplayInfo();
+        }
+    }
+}
---
+++ lab6/Lab6z2v1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6z2v1
+{
+    struct Detals
+    {
+        public string name_spare_parts;
+        public string car_brand;
+        public int graduation_year;
+
+        public string DisplayInfo()
+        {
+            return $"Name_spare_parts: {name_spare_parts},  Car_brand: {car_brand},  Graduation_year: {graduation_year};";
+        }
+    }
+    public partial class Lab6z2v1 : Form
+    {
+        public Lab6z2v1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Detals detal1;
+            detal1.name_spare_parts = textBox1.Text;
+            detal1.car_brand = textBox2.Text;
+            detal1.graduation_year = Convert.ToInt32(textBox3.Text);
+            textBox4.Text = detal1.DisplayInfo();
+
+            Detals detal2;
+            detal2.name_spare_parts = textBox5.Text;
+            detal2.car_brand = textBox6.Text;
+            detal2.graduation_year = Convert.ToInt32(textBox7.Text);
+            textBox8.Text = detal2.DisplayInfo();
+        }
+    }
+}
