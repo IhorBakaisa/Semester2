8850be0e441ac28f6af8166a30d3e3ec8042215f
IhorBakaisa <73829374+IhorBakaisa@users.noreply.github.com>
2021-05-22 20:03:35 +0300

    Lab14

---
+++ Lab14v1/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSimulation
+{
+    class Book
+    {
+
+        public string Authors { get; set; }
+        public string NameBook { get; set; }
+        public string Publishing_house { get; set; }
+        public string Library_section { get; set; }
+        public string Origin_book { get; set; }
+        public string Availability { get; set; }
+    
+        public Book(string Authors, string NameBook, string Publishing_house, string Library_section, string Origin_book, string Availability)
+        {
+            this.Authors = Authors;
+            this.NameBook = NameBook;
+            this.Publishing_house = Publishing_house;
+            this.Library_section = Library_section;
+            this.Origin_book = Origin_book;
+            this.Availability = Availability;
+        }
+    }
+}
---
+++ Lab14v1/BookDatabase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSimulation
+{
+    class BookDatabase
+    {
+        private List<Book> _Db = new List<Book>();
+
+        //додати
+        public void Add(Book book)
+        {
+            _Db.Add(book);
+        }
+
+        //вилучити
+        public void Remove(Book book)
+        {
+            _Db.Remove(book);
+        }
+
+        //дізнатись кількість записів в базі
+        public int Count
+        {
+            get
+            {
+                return _Db.Count;
+            }
+        }
+
+        public List<Book> Query(Func<Book, bool> condition)
+        {
+            return _Db.Where(condition).ToList();
+        }
+
+        public List<Book> GetAll()
+        {
+            return this._Db;
+        }
+
+        public Book GetByIndex(int index)
+        {
+            return this._Db[index];
+        }
+
+    }
+}
